b4ca3876f8e6d0cce039d5156e2c47a7ce854afc
Key5n <k.nogami.472@stn.nitech.ac.jp>
2024-11-11 05:11:39 +0900

    feat: enable to save models and global precision matrix trained on all clients

--- FedMDRS/__main__.py
+++ FedMDRS/__main__.py
@@ -1,4 +1,6 @@
 import os
+import pickle
+import numpy as np
 from utils import train_in_clients, evaluate_in_clients
 
 dirname = "ServerMachineDataset"
@@ -6,8 +8,20 @@ train_data_dir_path = os.path.join(dirname, "train")
 test_data_dir_path = os.path.join(dirname, "test")
 test_label_dir_path = os.path.join(dirname, "test_label")
 
-models_dic = {}
+train = True
+save = True
 
-global_precision_matrix = train_in_clients(models_dic, train_data_dir_path)
+if train:
+    models_dic = {}
+    P_global = train_in_clients(models_dic, train_data_dir_path)
 
-evaluate_in_clients(global_precision_matrix, models_dic, test_data_dir_path, test_label_dir_path)
+    if save:
+        with open("models.pickle", "wb") as f:
+            pickle.dump(models_dic, f, pickle.HIGHEST_PROTOCOL)
+        np.savetxt("P_global.txt", P_global, delimiter=",")
+else:
+    P_global = np.genfromtxt("P_global.txt", dtype=np.float64, delimiter=",")
+    with open("models.pickle", "rb") as f:
+        models_dic = pickle.load(f)
+
+evaluate_in_clients(P_global, models_dic, test_data_dir_path, test_label_dir_path)
