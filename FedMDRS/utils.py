fix: fix global precision matrix update expression

FedMDRS/utils.py
@@ -89,7 +89,7 @@ def write_roc_curve(fprs, tprs, auc, filename):
 
 def update_global_P(P_global: NDArray, local_updates: NDArray):
     top = np.dot(np.dot(P_global, local_updates), P_global)
-    bottom = np.trace(np.dot(local_updates, P_global))
+    bottom = 1 + np.trace(np.dot(local_updates, P_global))
     P_global = P_global - top / bottom
 
     return P_global