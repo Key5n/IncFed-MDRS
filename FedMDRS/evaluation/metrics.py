665c1fcf4554f7744722a0c9cf7686b0a288dc3c
Key5n <k.nogami.472@stn.nitech.ac.jp>
2024-12-16 11:05:37 +0900

    fix: delete metrics not to be used

--- FedMDRS/evaluation/metrics.py
+++ FedMDRS/evaluation/metrics.py
@@ -21,27 +21,10 @@ def get_metrics(score, labels, slidingWindow=100, pred=None, version="opt", thre
     # PATE returns floating[Any] or float, so forces float
     pate = float(PATE(labels, score, binary_scores=False))
 
-    """
-    Threshold Dependent
-    if pred is None --> use the oracle threshold
-    """
-
-    PointF1 = grader.metric_PointF1(labels, score, preds=pred)
-    PointF1PA = grader.metric_PointF1PA(labels, score, preds=pred)
-    EventF1PA = grader.metric_EventF1PA(labels, score, preds=pred)
-    RF1 = grader.metric_RF1(labels, score, preds=pred)
-    Affiliation_F = grader.metric_Affiliation(labels, score, preds=pred)
-
     metrics["AUC-PR"] = AUC_PR
     metrics["AUC-ROC"] = AUC_ROC
     metrics["VUS-PR"] = VUS_PR
     metrics["VUS-ROC"] = VUS_ROC
     metrics["PATE"] = pate
-
-    metrics["Standard-F1"] = PointF1
-    metrics["PA-F1"] = PointF1PA
-    metrics["Event-based-F1"] = EventF1PA
-    metrics["R-based-F1"] = RF1
-    metrics["Affiliation-F"] = Affiliation_F
 
     return metrics
