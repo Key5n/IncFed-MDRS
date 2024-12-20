66e14225548e8900783b0f49006dc41011bb5b8b
Key5n <k.nogami.472@stn.nitech.ac.jp>
2024-12-20 10:21:29 +0900

    chore: measure consumed time on calculating each metric

--- FedMDRS/evaluation/metrics.py
+++ FedMDRS/evaluation/metrics.py
@@ -1,3 +1,4 @@
+import time
 from pate.PATE_metric import PATE
 from .basic_metrics import basic_metricor, generate_curve
 
@@ -10,16 +11,30 @@ def get_metrics(score, labels, slidingWindow=100, pred=None, version="opt", thre
     """
     grader = basic_metricor()
     # AUC_ROC, Precision, Recall, PointF1, PointF1PA, Rrecall, ExistenceReward, OverlapReward, Rprecision, RF, Precision_at_k = grader.metric_new(labels, score, pred, plot_ROC=False)
+    auc_roc_start = time.time()
     AUC_ROC = grader.metric_ROC(labels, score)
+    auc_roc_end = time.time()
+
+    auc_pr_start = time.time()
     AUC_PR = grader.metric_PR(labels, score)
+    auc_pr_end = time.time()
 
     # R_AUC_ROC, R_AUC_PR, _, _, _ = grader.RangeAUC(labels=labels, score=score, window=slidingWindow, plot_ROC=True)
+    vus_start = time.time()
     _, _, _, _, _, _, VUS_ROC, VUS_PR = generate_curve(
         labels, score, slidingWindow, version, thre
     )
+    vus_end = time.time()
 
     # PATE returns floating[Any] or float, so forces float
+    pate_start = time.time()
     pate = float(PATE(labels, score, binary_scores=False, n_jobs=-1))
+    pate_end = time.time()
+
+    print(f"AUCROC Time: {auc_roc_end - auc_roc_start}")
+    print(f"AUCPR Time: {auc_pr_end - auc_pr_start}")
+    print(f"VUS Time: {vus_end - vus_start}")
+    print(f"PATE Time: {pate_end - pate_start}")
 
     metrics["AUC-PR"] = AUC_PR
     metrics["AUC-ROC"] = AUC_ROC
