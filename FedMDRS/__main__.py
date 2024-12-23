3384fda7e75e9aff94cf42cc555e5e70acbe3f2a
Key5n <k.nogami.472@stn.nitech.ac.jp>
2024-12-23 12:29:38 +0900

    fix: evaluation in server

--- FedMDRS/__main__.py
+++ FedMDRS/__main__.py
@@ -2,57 +2,31 @@ import os
 
 import numpy as np
 from utils.datasets import create_SMD_train, create_SMD_test
-from utils.optimize import optimize_federated_HP, optimize_isolated_HP
-
-import json
 
 from utils.utils import (
-    evaluate_in_client,
-    evaluate_in_clients,
-    train_in_client,
+    evaluate,
     train_in_clients,
 )
 
 train = True
-isolated = False
 federated = True
 save = True
-optimize = False
 
-serverMachineDataset = create_SMD_train()
+dataset = create_SMD_train()
+X_test, y_test = create_SMD_test()
 
 if federated:
     output_dir = os.path.join("result", "federated")
     os.makedirs(output_dir, exist_ok=True)
+
+    leaking_rate = 1.0
+    delta = 0.0001
+    rho = 0.95
+    input_scale = 1.0
+
     if train:
-        if optimize:
-            leaking_rate, delta, rho, input_scale = optimize_federated_HP(
-                serverMachineDataset, output_dir
-            )
-
-            with open(os.path.join(output_dir, "best_params.json"), "w") as f:
-                best_params = {}
-                best_params["leaking_rate"] = leaking_rate
-                best_params["delta"] = delta
-                best_params["rho"] = rho
-                best_params["input_scale"] = input_scale
-
-                json.dump(best_params, f)
-        elif os.path.isfile(os.path.join(output_dir, "best_params.json")):
-            with open(os.path.join(output_dir, "best_params.json"), "r") as f:
-                loaded_best_params = json.load(f)
-                leaking_rate = loaded_best_params.leaking_rate
-                delta = loaded_best_params.delta
-                rho = loaded_best_params.rho
-                input_scale = loaded_best_params.input_scale
-        else:
-            leaking_rate = 1.0
-            delta = 0.0001
-            rho = 0.95
-            input_scale = 1.0
-
         P_global = train_in_clients(
-            serverMachineDataset,
+            dataset,
             leaking_rate=leaking_rate,
             delta=delta,
             rho=rho,
@@ -67,82 +41,18 @@ if federated:
         with open(os.path.join(output_dir, "P_global.npy"), "rb") as f:
             P_global = np.load(f)
 
-    auc_roc_avg, auc_pr_avg, vus_roc_avg, vus_pr_avg, pate_avg = evaluate_in_clients(
-        serverMachineDataset, P_global, output_dir
+    auc_roc, auc_pr, vus_roc, vus_pr, pate = evaluate(
+        X_test,
+        y_test,
+        P_global,
+        leaking_rate=leaking_rate,
+        delta=delta,
+        input_scale=input_scale,
+        rho=rho,
     )
 
-    print(f"{auc_roc_avg = }")
-    print(f"{auc_pr_avg = }")
-    print(f"{vus_roc_avg = }")
-    print(f"{vus_pr_avg = }")
-    print(f"{pate_avg = }")
-
-if isolated:
-    print("isolated")
-    output_dir = os.path.join("result", "isolated")
-    os.makedirs(output_dir, exist_ok=True)
-
-    for serverMachineData in serverMachineDataset:
-        output_dir_client = os.path.join(output_dir, serverMachineData.entity_name)
-        os.makedirs(os.path.join(output_dir_client), exist_ok=True)
-
-        if train:
-            if optimize:
-                leaking_rate, delta, rho, input_scale = optimize_isolated_HP(
-                    serverMachineDataset, output_dir_client
-                )
-
-                with open(os.path.join(output_dir, "best_params.json"), "w") as f:
-                    best_params = {}
-                    best_params["leaking_rate"] = leaking_rate
-                    best_params["delta"] = delta
-                    best_params["rho"] = rho
-                    best_params["input_scale"] = input_scale
-
-                    json.dump(best_params, f)
-            elif os.path.isfile(os.path.join(output_dir_client, "best_params.json")):
-                with open(
-                    os.path.join(output_dir_client, "best_params.json"), "r"
-                ) as f:
-                    loaded_best_params = json.load(f)
-                    leaking_rate = loaded_best_params.leaking_rate
-                    delta = loaded_best_params.delta
-                    rho = loaded_best_params.rho
-                    input_scale = loaded_best_params.input_scale
-            else:
-                leaking_rate = 1.0
-                delta = 0.0001
-                rho = 0.95
-                input_scale = 1.0
-
-            covariance_matrix = train_in_client(
-                serverMachineData,
-                leaking_rate=leaking_rate,
-                delta=delta,
-                rho=rho,
-                input_scale=input_scale,
-            )
-            P = np.linalg.inv(covariance_matrix)
-
-            if save:
-                with open(
-                    os.path.join(output_dir_client, "P.npy"),
-                    "wb",
-                ) as f:
-                    np.save(f, P)
-        else:
-            with open(
-                os.path.join(output_dir_client, "P.npy"),
-                "rb",
-            ) as f:
-                P = np.load(f)
-
-        auc_roc, auc_pr, vus_roc, vus_pr, pate = evaluate_in_client(
-            serverMachineData, P, output_dir=output_dir
-        )
-
-        print(f"{auc_roc = }")
-        print(f"{auc_pr = }")
-        print(f"{vus_roc = }")
-        print(f"{vus_pr = }")
-        print(f"{pate = }")
+    print(f"{auc_roc = }")
+    print(f"{auc_pr = }")
+    print(f"{vus_roc = }")
+    print(f"{vus_pr = }")
+    print(f"{pate = }")
--- FedMDRS/utils/utils.py
+++ FedMDRS/utils/utils.py
@@ -55,73 +55,35 @@ def train_in_client(
     return local_updates
 
 
-def evaluate_in_clients(
-    serverMachineDataset,
+def evaluate(
+    X_test: NDArray,
+    y_test: NDArray,
     P_global: NDArray,
-    output_dir: str,
-) -> tuple[float, float, float, float, float]:
-    auc_rocs = []
-    auc_prs = []
-    vus_rocs = []
-    vus_prs = []
-    pates = []
-    for i, serverMachineData in enumerate(serverMachineDataset):
-        print(f"Progress Rate: {i / len(serverMachineDataset):.1%}")
-
-        auc_roc, auc_pr, vus_roc, vus_pr, pate = evaluate_in_client(
-            serverMachineData, P_global, output_dir
-        )
-        auc_rocs.append(auc_roc)
-        auc_prs.append(auc_pr)
-        vus_rocs.append(vus_roc)
-        vus_prs.append(vus_pr)
-        pates.append(pate)
-
-    auc_roc_avg = np.mean(auc_rocs, dtype=float)
-    auc_pr_avg = np.mean(auc_prs, dtype=float)
-    vus_roc_avg = np.mean(vus_rocs, dtype=float)
-    vus_pr_avg = np.mean(vus_prs, dtype=float)
-    pate_avg = np.mean(pates, dtype=float)
-
-    return auc_roc_avg, auc_pr_avg, vus_roc_avg, vus_pr_avg, pate_avg
-
-
-def evaluate_in_client(
-    serverMachineData,
-    P: NDArray,
-    output_dir: str,
     leaking_rate: float = 1.0,
     rho: float = 0.95,
     delta: float = 0.0001,
     input_scale: float = 1.0,
 ) -> tuple[float, float, float, float, float]:
-    name = serverMachineData.data_name
-    os.makedirs(os.path.join(output_dir, name), exist_ok=True)
-    with open(os.path.join(output_dir, name, "log.txt"), "w") as f:
-        print(f"[test] dataset name: {name}", file=f)
-        print(f"[test] dataset name: {name}")
 
-        label_test = serverMachineData.test_label
-        data_test = serverMachineData.data_test
+    N_u = X_test.shape[1]
+    N_x = 200
+    model = MDRS(
+        N_u,
+        N_x,
+        precision_matrix=P_global,
+        leaking_rate=leaking_rate,
+        delta=delta,
+        rho=rho,
+        input_scale=input_scale,
+    )
+    mahalanobis_distances = model.adapt(X_test)
 
-        N_u = data_test.shape[1]
-        N_x = 200
-        model = MDRS(
-            N_u,
-            N_x,
-            precision_matrix=P,
-            leaking_rate=leaking_rate,
-            delta=delta,
-            rho=rho,
-            input_scale=input_scale,
-        )
-        mahalanobis_distances = model.adapt(data_test)
+    evaluation_result = get_metrics(mahalanobis_distances, y_test)
 
-        evaluation_result = get_metrics(mahalanobis_distances, label_test)
-        auc_roc = evaluation_result["AUC-ROC"]
-        auc_pr = evaluation_result["AUC-PR"]
-        vus_roc = evaluation_result["VUS-ROC"]
-        vus_pr = evaluation_result["VUS-PR"]
-        pate = evaluation_result["PATE"]
+    auc_roc = evaluation_result["AUC-ROC"]
+    auc_pr = evaluation_result["AUC-PR"]
+    vus_roc = evaluation_result["VUS-ROC"]
+    vus_pr = evaluation_result["VUS-PR"]
+    pate = evaluation_result["PATE"]
 
     return auc_roc, auc_pr, vus_roc, vus_pr, pate
