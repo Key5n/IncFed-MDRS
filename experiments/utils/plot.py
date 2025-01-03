ba640e5c267d94807d94c287389651cb5a20cef9
Key5n <k.nogami.472@stn.nitech.ac.jp>
2025-01-03 15:27:56 +0900

    fix: highlight anomalies by background color

--- experiments/utils/plot.py
+++ experiments/utils/plot.py
@@ -1,27 +1,51 @@
-import numpy as np
 import matplotlib.pyplot as plt
 from numpy.typing import NDArray
 
 
-def plot(anomaly_scores: NDArray, true_labels: NDArray, filename: str):
+def range_convers_new(label):
+    """
+    input: arrays of binary values
+    output: list of ordered pair [[a0,b0], [a1,b1]... ] of the inputs
+    """
+    L = []
+    i = 0
+    j = 0
+    while j < len(label):
+        # print(i)
+        while label[i] == 0:
+            i += 1
+            if i >= len(label):
+                break
+        j = i + 1
+        # print('j'+str(j))
+        if j >= len(label):
+            if j == len(label):
+                L.append((i, j - 1))
 
-    # Create a figure and axis
+            break
+        while label[j] != 0:
+            j += 1
+            if j >= len(label):
+                L.append((i, j - 1))
+                break
+        if j >= len(label):
+            break
+        L.append((i, j - 1))
+        i = j
+    return L
+
+
+def plot(scores: NDArray, labels: NDArray, filename: str):
+    range_anomaly = range_convers_new(labels)
+
+    # # Create a figure and axis
     _, ax = plt.subplots(figsize=(10, 6))
 
     # Plot the anomaly scores
-    ax.plot(
-        anomaly_scores, label="Anomaly Scores", color="blue", linestyle="-", marker="o"
-    )
+    ax.plot(scores, label="Anomaly Scores", color="blue", linestyle="-")
 
-    # Highlight true labels as a scatter plot
-    anomalies = np.where(true_labels == 1)[0]
-    ax.scatter(
-        anomalies,
-        anomaly_scores[anomalies],
-        color="red",
-        label="Anomalies (True Labels)",
-        zorder=5,
-    )
+    for r in range_anomaly:
+        ax.axvspan(r[0], r[1], color="red", alpha=0.5)
 
     # Add grid, labels, and title
     ax.grid(True, linestyle="--", alpha=0.6)
@@ -34,4 +58,4 @@ def plot(anomaly_scores: NDArray, true_labels: NDArray, filename: str):
 
     # Show the plot
     plt.tight_layout()
-    plt.savefig(filename)
+    plt.saveFig(filename)
