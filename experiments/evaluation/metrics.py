5388d8feb05d6ef1c058cc7b15e2609e3a99452f
Key5n <k.nogami.472@stn.nitech.ac.jp>
2025-01-08 14:30:22 +0900

    fix: not log metric calculation time by default

--- experiments/evaluation/metrics.py
+++ experiments/evaluation/metrics.py
@@ -4,7 +4,9 @@ from pate.PATE_metric import PATE
 from .basic_metrics import basic_metricor, generate_curve
 
 
-def get_metrics(score, labels, slidingWindow=100, pred=None, version="opt", thre=250):
+def get_metrics(
+    score, labels, slidingWindow=100, pred=None, version="opt", thre=250, log=False
+):
     logger = getLogger(__name__)
     metrics = {}
 
@@ -16,12 +18,14 @@ def get_metrics(score, labels, slidingWindow=100, pred=None, version="opt", thre
     auc_roc_start = time.time()
     AUC_ROC = grader.metric_ROC(labels, score)
     auc_roc_end = time.time()
-    logger.info(f"AUCROC Time: {auc_roc_end - auc_roc_start}")
+    if log:
+        logger.info(f"AUCROC Time: {auc_roc_end - auc_roc_start}")
 
     auc_pr_start = time.time()
     AUC_PR = grader.metric_PR(labels, score)
     auc_pr_end = time.time()
-    logger.info(f"AUCPR Time: {auc_pr_end - auc_pr_start}")
+    if log:
+        logger.info(f"AUCPR Time: {auc_pr_end - auc_pr_start}")
 
     # R_AUC_ROC, R_AUC_PR, _, _, _ = grader.RangeAUC(labels=labels, score=score, window=slidingWindow, plot_ROC=True)
     vus_start = time.time()
@@ -29,13 +33,15 @@ def get_metrics(score, labels, slidingWindow=100, pred=None, version="opt", thre
         labels, score, slidingWindow, version, thre
     )
     vus_end = time.time()
-    logger.info(f"VUS Time: {vus_end - vus_start}")
+    if log:
+        logger.info(f"VUS Time: {vus_end - vus_start}")
 
     # PATE returns floating[Any] or float, so forces float
     pate_start = time.time()
     pate = float(PATE(labels, score, binary_scores=False, n_jobs=-1))
     pate_end = time.time()
-    logger.info(f"PATE Time: {pate_end - pate_start}")
+    if log:
+        logger.info(f"PATE Time: {pate_end - pate_start}")
 
     metrics["AUC-PR"] = AUC_PR
     metrics["AUC-ROC"] = AUC_ROC
