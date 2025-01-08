8b15ac78e9102a7502fcc4f7c87854aa021c507a
Key5n <k.nogami.472@stn.nitech.ac.jp>
2025-01-08 16:16:48 +0900

    fix: lstmae boxplot

--- lstmae_main.py
+++ lstmae_main.py
@@ -79,5 +79,4 @@ if __name__ == "__main__":
         evaluation_result = get_metrics(scores, labels)
         evaluation_results.append(evaluation_result)
 
-    filename = os.path.join(result_dir, "boxplot.pdf")
-    get_final_scores(evaluation_results, filename)
+    get_final_scores(evaluation_results, result_dir)
