1e8077ff0514375a1cad01bab4b8ee70a93a09ea
Key5n <k.nogami.472@stn.nitech.ac.jp>
2025-01-08 14:37:56 +0900

    feat: show tqdm for each client

--- tranad_main.py
+++ tranad_main.py
@@ -1,5 +1,5 @@
 import os
-from tqdm import trange
+from tqdm import trange, tqdm
 import torch
 from torch import nn
 from experiments.utils.logger import init_logger
@@ -52,7 +52,7 @@ if __name__ == "__main__":
         model.fit(train_dataloader, epoch)
 
     evaluation_results = []
-    for i, test_dataloader in enumerate(test_dataloader_list):
+    for i, test_dataloader in tqdm(enumerate(test_dataloader_list)):
         scores = model.run(test_dataloader)
         labels = getting_labels_for_TranAD(test_dataloader)
 
