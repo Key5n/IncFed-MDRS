b46082df491c70cda66d42fef3f4f3948aaf3db6
Key5n <k.nogami.472@stn.nitech.ac.jp>
2025-01-09 18:26:54 +0900

    feat: USAD fedavg

---
+++ experiments/algorithms/USAD/fed_utils.py
@@ -0,0 +1,52 @@
+import os
+from experiments.algorithms.USAD.federated import UsadClient
+from experiments.algorithms.USAD.utils import generate_train_loader
+import numpy as np
+from numpy.typing import NDArray
+
+
+def get_SMD_data(data_dir_path: str) -> list[NDArray]:
+    data_filenames = os.listdir(data_dir_path)
+
+    data_list = []
+    for data_filename in data_filenames:
+        data_file_path = os.path.join(data_dir_path, data_filename)
+
+        data = np.genfromtxt(data_file_path, dtype=np.float64, delimiter=",")
+
+        data_list.append(data)
+
+    return data_list
+
+
+def get_SMD_clients_Usad(
+    optimizer,
+    w_size: int,
+    z_size: int,
+    local_epochs: int,
+    device,
+    batch_size,
+    window_size,
+    train_data_dir_path: str = os.path.join(
+        os.getcwd(), "datasets", "ServerMachineDataset", "train"
+    ),
+    seed=0,
+):
+    dataset_name = "SMD"
+    X_train_list = get_SMD_data(train_data_dir_path)
+
+    clients = []
+    for i, X_train in enumerate(X_train_list):
+        train_dataloader = generate_train_loader(X_train, batch_size, window_size, seed)
+        client = UsadClient(
+            f"{dataset_name}-client-{i}",
+            train_dataloader,
+            optimizer,
+            w_size,
+            z_size,
+            local_epochs,
+            device,
+        )
+        clients.append(client)
+
+    return clients
---
+++ experiments/algorithms/USAD/federated.py
@@ -0,0 +1,78 @@
+from logging import getLogger
+import numpy as np
+from tqdm import trange
+from tqdm.contrib.logging import logging_redirect_tqdm
+from experiments.algorithms.USAD.usad import UsadModel
+from experiments.utils.utils import to_device
+import torch
+from torch.utils.data import DataLoader
+
+
+class UsadClient:
+    def __init__(
+        self,
+        client_name: str,
+        train_dataloader: DataLoader,
+        optimizer_generate_function,
+        w_size: int,
+        z_size: int,
+        local_epochs: int,
+        device: str,
+    ):
+        self.client_name = client_name
+        self.train_dataloader = train_dataloader
+        self.optimizer_generate_function = optimizer_generate_function
+        self.w_size = w_size
+        self.z_size = z_size
+        self.local_epochs = local_epochs
+        self.device = device
+
+    def train_avg(self, global_state_dict):
+        logger = getLogger(__name__)
+
+        model = UsadModel(self.w_size, self.z_size)
+        model.load_state_dict(global_state_dict)
+        model.to(self.device)
+
+        optimizer1 = self.optimizer_generate_function(
+            list(model.encoder.parameters()) + list(model.decoder1.parameters())
+        )
+        optimizer2 = self.optimizer_generate_function(
+            list(model.encoder.parameters()) + list(model.decoder2.parameters())
+        )
+
+        model.train()
+
+        for epoch in trange(self.local_epochs):
+            loss1_list = []
+            loss2_list = []
+            for batch in self.train_dataloader:
+                batch = torch.flatten(batch[0], start_dim=1)  # I added this
+                batch = to_device(batch, self.device)
+
+                # Train AE1
+                loss1, loss2 = model.training_step(batch, epoch + 1)
+                loss1.backward()
+                optimizer1.step()
+                optimizer1.zero_grad()
+
+                loss1_list.append(loss1.item())
+                loss2_list.append(loss2.item())
+
+                # Train AE2
+                loss1, loss2 = model.training_step(batch, epoch + 1)
+
+                loss2.backward()
+                optimizer2.step()
+                optimizer2.zero_grad()
+
+                loss1_list.append(loss1.item())
+                loss2_list.append(loss2.item())
+
+            with logging_redirect_tqdm():
+                logger.info(
+                    f"Epoch {epoch}, loss1: {np.mean(loss1_list)}, loss2: {np.mean(loss2_list)}"
+                )
+
+        data_num = len(next(iter(self.train_dataloader)))
+        return model.state_dict(), data_num
