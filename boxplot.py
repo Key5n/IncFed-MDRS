f71b4d8cc493f1d42b4b8110b39acf87c4dcd632
Key5n <k.nogami.472@stn.nitech.ac.jp>
2025-01-23 03:43:25 +0900

    fix: boxplot

--- boxplot.py
+++ boxplot.py
@@ -24,7 +24,8 @@ def boxplot(X, tick_labels: list[str], colors: list[tuple], y_label: str):
 
     for patch, color in zip(bplot["boxes"], colors):
         patch.set_facecolor(color)
-
+    plt.xticks(rotation=30)
+    plt.tight_layout()
     plt.show()
 
     plt.clf()
@@ -36,21 +37,20 @@ tick_labels = [
     "TranAD",
     "FedAvg TranAD",
     "LSTM-AE",
-    "FedAvg LSTM-AE",
+    # "FedAvg LSTM-AE",
     "IncFed ESN-SRE",
     "FedAvg MDRS",
-    "MDRS",
 ]
 colors = [
     (216 / 255, 85 / 255, 255 / 255, 1 - i / len(tick_labels))
     for i in range(len(tick_labels))
 ]
-dataset = "PSM"
+dataset = "SMD"
 metric = "pate"
 
 X = [
     np.genfromtxt(
-        os.path.join(os.getcwd(), f"result/mdrs/proposed/{dataset}/{metric}.csv"),
+        os.path.join(os.getcwd(), f"result/mdrs/proposed/{dataset}/{metric}.csv")
     ),
     np.genfromtxt(
         os.path.join(os.getcwd(), f"result/tranad/centralized/{dataset}/{metric}.csv")
@@ -61,19 +61,17 @@ X = [
     np.genfromtxt(
         os.path.join(os.getcwd(), f"result/lstmae/centralized/{dataset}/{metric}.csv")
     ),
-    np.genfromtxt(
-        os.path.join(os.getcwd(), f"result/lstmae/fedavg/{dataset}/{metric}.csv")
-    ),
+    # np.genfromtxt(
+    #     os.path.join(os.getcwd(), f"result/lstmae/fedavg/{dataset}/{metric}.csv")
+    # ),
     np.genfromtxt(
         os.path.join(os.getcwd(), f"result/ESN-SRE/IncFed/{dataset}/{metric}.csv")
     ),
     np.genfromtxt(
         os.path.join(os.getcwd(), f"result/mdrs/fedavg/{dataset}/{metric}.csv")
     ),
-    np.genfromtxt(
-        os.path.join(os.getcwd(), f"result/mdrs/proposed/{dataset}/{metric}.csv")
-    ),
 ]
+
 
 boxplot(
     X,
