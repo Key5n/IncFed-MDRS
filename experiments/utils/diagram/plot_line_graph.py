35597bf2760aa5333147dbff49d84060442c100c
Key5n <k.nogami.472@stn.nitech.ac.jp>
2025-01-23 20:23:01 +0900

    fix: plot_line_graph shows legend

--- experiments/utils/diagram/plot_line_graph.py
+++ experiments/utils/diagram/plot_line_graph.py
@@ -2,14 +2,15 @@ import matplotlib.pyplot as plt
 
 
 def plot_line_graph(X: list, Y_list: list[list], label_list: list[str], filepath: str):
-    for Y, legend in zip(Y_list, label_list):
-        plt.plot(X, Y, label=legend)
+    for Y, label in zip(Y_list, label_list):
+        plt.plot(X, Y, label=label)
 
     plt.xlabel("Client Size")
     plt.ylabel("Performance")
 
     plt.ylim(0, 1)
 
+    plt.legend()
     plt.tight_layout()
     plt.savefig(filepath)
 
