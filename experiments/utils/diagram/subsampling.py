11c4a15a9eef53d13564eb2987f55185410ffa36
Key5n <k.nogami.472@stn.nitech.ac.jp>
2025-01-24 19:55:19 +0900

    fix(subsampling): fix figure

--- experiments/utils/diagram/subsampling.py
+++ experiments/utils/diagram/subsampling.py
@@ -1,19 +1,29 @@
 import numpy as np
+from numpy.typing import NDArray
 import matplotlib.pyplot as plt
 
 
-def plot_subsampling(X, Y, stds, filename):
+def plot_subsampling(
+    X: list,
+    Y_list: list[NDArray],
+    stds_list: list[list[float]],
+    labels: list[str],
+    filename: str,
+):
     X = np.array(X)
-    Y = np.array(Y)
-    stds = np.array(stds)
 
-    plt.plot(X, Y, marker="o")
+    for Y, stds, label in zip(Y_list, stds_list, labels):
+        Y = np.array(Y)
+        stds = np.array(stds)
+
+        plt.plot(X, Y, marker="o", label=label)
+        plt.fill_between(X, Y + stds, Y - stds, alpha=0.15)
+
     plt.xlabel("Subsampling size")
     plt.ylabel("Performance")
+    plt.legend()
 
     plt.ylim(0, 1)
-
-    plt.fill_between(X, Y + stds, Y - stds, alpha=0.15)
 
     plt.tight_layout()
     plt.savefig(filename)
--- vary_subsampling.py
+++ vary_subsampling.py
@@ -1,45 +1,70 @@
-from logging import getLogger
 import os
 from experiments.utils.diagram.subsampling import plot_subsampling
 import numpy as np
 from experiments.utils.logger import init_logger
-from experiments.utils.parser import args_parser
 from fedmdrs_main import fedmdrs_main
 
 
 if __name__ == "__main__":
-    args = args_parser()
-    dataset = args.dataset
-    result_dir = os.path.join("result", "mdrs", "subsampling", dataset)
-    os.makedirs(result_dir, exist_ok=True)
+    datasets = ["SMD", "SMAP", "PSM"]
+    result_dir = os.path.join("result", "mdrs", "subsampling")
     init_logger(os.path.join(result_dir, "mdrs.log"))
-    logger = getLogger(__name__)
-
-    N_x = 500
 
     subsampling_sizes = [10, 25, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500]
-    run = True
+    scores_list = []
+    stds_list = []
+    for dataset in datasets:
+        result_dir_for_each_dataset = os.path.join(result_dir, dataset)
+        os.makedirs(result_dir_for_each_dataset, exist_ok=True)
 
-    if run:
-        scores = [
-            fedmdrs_main(
-                dataset,
-                result_dir=os.path.join(result_dir, str(N_x_tilde)),
-                N_x=N_x,
-                N_x_tilde=N_x_tilde,
-            )
-            for N_x_tilde in subsampling_sizes
-        ]
-    else:
-        scores = [
-            np.mean(np.genfromtxt(os.path.join(result_dir, str(N_x_tilde), "pate.csv")))
-            for N_x_tilde in subsampling_sizes
-        ]
+        N_x = 500
+        run = False
+        if run:
+            scores = [
+                fedmdrs_main(
+                    dataset,
+                    result_dir=os.path.join(
+                        result_dir_for_each_dataset, str(N_x_tilde)
+                    ),
+                    N_x=N_x,
+                    N_x_tilde=N_x_tilde,
+                )
+                for N_x_tilde in subsampling_sizes
+            ]
+        else:
+            scores = [
+                np.mean(
+                    np.genfromtxt(
+                        os.path.join(
+                            result_dir_for_each_dataset, str(N_x_tilde), "pate.csv"
+                        )
+                    )
+                )
+                for N_x_tilde in subsampling_sizes
+            ]
 
-    pate_stds = [
-        np.std(np.genfromtxt(os.path.join(result_dir, str(N_x_tilde), "pate.csv")))
-        for N_x_tilde in subsampling_sizes
-    ]
+        if dataset != "PSM":
+            pate_stds = [
+                np.std(
+                    np.genfromtxt(
+                        os.path.join(
+                            result_dir_for_each_dataset, str(N_x_tilde), "pate.csv"
+                        )
+                    )
+                )
+                for N_x_tilde in subsampling_sizes
+            ]
+        else:
+            pate_stds = np.zeros(len(subsampling_sizes))
 
-    diagram_path = os.path.join(result_dir, "diagram.pdf")
-    plot_subsampling(subsampling_sizes, scores, pate_stds, filename=diagram_path)
+        scores_list.append(scores)
+        stds_list.append(pate_stds)
+
+    diagram_path = os.path.join(result_dir, "subsampling-result.pdf")
+    plot_subsampling(
+        subsampling_sizes,
+        scores_list,
+        stds_list,
+        labels=datasets,
+        filename=diagram_path,
+    )
